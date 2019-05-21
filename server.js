f3ad56132c82331fe4da6b840d2e0bd44b46af22
Jared Cole-Lewis <jaredcole-lewis@Jareds-Air.fios-router.home>
2019-05-21 14:13:04 -0400

    Added basic update, and add Routes.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const {router} = require("./controllers/burgers_controller");
 
+const {addAburger, getAllburgers, updateABurger} = require("./models/burger")
+
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -18,10 +20,26 @@ res.render('index');
 
 });
 
+app.post('/', (req, res) => {
+
+addAburger(req[0]);    
+
+res.status(200);
+
+})
+
+app.put('/', (req, res) => {
+updateABurger(req[0])
+
+res.status(200)
+})
+
 app.listen(port, () => {
 
+       
     console.log(`app is runnig -> PORT ${port}`);
     
     });
+
 
     
