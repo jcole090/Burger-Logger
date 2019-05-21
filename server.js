531ed1d12117de081a3661d0c5770c535cca1c04
Jared Cole-Lewis <jaredcole-lewis@Jareds-Air.fios-router.home>
2019-05-20 20:13:31 -0400

    It works!

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 
 const {router} = require("./controllers/burgers_controller");
 
-const {app} = require("./views/index.handlebars");
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+
+res.render('index');
+
+});
 
 app.listen(3000, () => {
 
